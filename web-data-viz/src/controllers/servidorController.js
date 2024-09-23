c872f49692ca870b8104e1c0f36f690555ef1bad
ViniciusMiralha <vinicius.gomes@sptech.school>
2024-09-23 14:16:32 -0300

    Alteração de campos - cadastro de servidor

--- web-data-viz/src/controllers/servidorController.js
+++ web-data-viz/src/controllers/servidorController.js
@@ -2,21 +2,16 @@ var servidorModel = require("../models/servidorModel");
 
 function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
-    var id = req.body.idServer;
     var placaderede = req.body.placaderedeServer;
-    var senha = req.body.senhaServer;
     var memoria = req.body.memoriaServer;
     var sistemaoperacional = req.body.sistemaoperacionalServer;
     var cpu = req.body.cpuServer;
     var disco = req.body.discoServer;
     
     // Faça as validações dos valores
-    if (id == undefined) {
-        res.status(400).send("Seu ID está indefinido!");
-    } else if (placaderede == undefined) {
-        res.status(400).send("Seu Placa de Rede está indefinida!");
-    } else if (senha == undefined) {
-        res.status(400).send("Sua senha está indefinida!");
+    
+    if (placaderede == undefined) {
+        res.status(400).send("Seu Placa de Rede está indefinida!");;
     } else if (memoria == undefined) {
         res.status(400).send("Sua memória está indefinida!");
     } else if (sistemaoperacional == undefined) {
@@ -28,7 +23,7 @@ function cadastrar(req, res) {
     } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        servidorModel.cadastrar(id, placaderede, senha, memoria, sistemaoperacional, cpu, disco)
+        servidorModel.cadastrar(placaderede, memoria, sistemaoperacional, cpu, disco)
             .then(
                 function (resultado) {
                     res.status(200).json(resultado);
--- web-data-viz/src/models/servidorModel.js
+++ web-data-viz/src/models/servidorModel.js
@@ -1,9 +1,9 @@
 var database = require("../database/config")
 
 
-function cadastrar(id, placaderede, senha, memoria, sistemaoperacional, cpu, disco) {
+function cadastrar( placaderede, memoria, sistemaOperacional, cpu, disco) {
     var instrucaoSql = `
-        INSERT INTO usuario (nome, email, senha, cargo, telefone, cpf, fkEmpresa) VALUES ('${id}', '${placaderede}', '${senha}', '${memoria}', '${sistemaoperacional}', '${cpu}', '${disco}');
+        INSERT INTO usuario (placaDeRede, qtd_totalmemoria, sistemaOperacional, qtd_cpu, qtd_totaldisco, fkEmpresa) VALUES ('${placaderede}', '${memoria}', '${sistemaOperacional}', '${cpu}', '${disco}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
