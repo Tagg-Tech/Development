b633d0d27f3ee80e49b042b57a84c22905893d41
raulreis123 <raulgomesreis@gmail.com>
2024-12-08 15:47:13 -0300

    feature(tomRoute): Adição de rota para busca de dados API em registro

--- web-data-viz/src/routes/tomtom.js
+++ web-data-viz/src/routes/tomtom.js
@@ -3,13 +3,20 @@ var express = require("express");
 var router = express.Router();
 var controller = require("../controllers/tomtomController");
 
+// Rota para atualização e envio de dados API
 router.get('/:condition', (req, res)=>{
     // Executar a consulta
     controller.consultarDadosTrafego(req, res);
 })
 
+// Rota para inserir dados da API em arquivo JSON
 router.post('/insertData', (req, res)=>{
     controller.writeFile(req, res);
+})
+
+router.get('/getData/:data', (req, res)=>{
+    console.log("Dados recebidos: " + req.params.data)
+    controller.getDataAPI(req, res);
 })
 
 module.exports = router;
